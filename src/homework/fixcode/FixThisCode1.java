b33cfe440b44d727e8662d6d3566d75057fc956e
SintiaTsolova <sintiatsolova@gmail.com>
2023-07-04 15:54:04 +0300

    add .FixThisCode1

--- src/homework/fixcode/FixThisCode1.java
+++ src/homework/fixcode/FixThisCode1.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class FixThisCode {
+public class FixThisCode1 {
 
         private static FileWriter getFileWriter(String filePath) {
             try {
